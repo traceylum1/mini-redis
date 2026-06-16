7a041b53fd7692e08b29685014d4e522eb54eefc
Tracey Lum <traceylum@gmail.com>
2026-06-15 17:04:26 -0700

    finish server event loop

--- 06_event-loop/server.cpp
+++ 06_event-loop/server.cpp
@@ -145,3 +145,156 @@ static void handle_write(Conn *conn) {
   // remove written data from 'outgoing'
   buf_consume(conn->outgoing, (size_t)rv);
   
+  // if all data from outgoing written, switch to read
+  if (conn->outgoing.size() == 0) {
+    conn->want_read = true;
+    conn->want_write = false;
+  }
+  return;
+}
+
+// application callback when the socket is readable
+static void handle_read(Conn *conn) {
+  // read some data
+  uint8_t buf[64 * 1024];
+  ssize_t rv = read(conn->fd, buf, sizeof(buf));
+  if (rv < 0 && errno == EAGAIN) {
+    return; // actually not ready
+  }
+  if (rv < 0) {
+    msg_errno("read() error");
+    conn->want_close = true;  // error handling
+    return;
+  }
+  
+  // handle EOF
+  if (rv == 0) {
+    if (conn->incoming.size() == 0) {
+      msg("client closed");
+    } else {
+      msg("unexpected EOF");
+    }
+    conn->want_close = true;
+    return;
+  }
+  
+  // got new data
+  buf_append(conn->incoming, buf, (size_t)rv);
+  
+  // parse requests and generate responses
+  while (try_one_request(conn)) {}
+  // Q: Why call this in a loop? See explanation of 'pipelining'
+  
+  // update the readiness intention
+  if (conn->outgoing.size() > 0) {
+    conn->want_read = true;
+    conn->want_write = false;
+    // The socket is likely ready to write in a request-response protocol
+    // try to write it without waiting for the next iteration
+    return handle_write(conn);
+  }
+}
+
+int main() {
+  // the listening socket
+  int fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (fd < 0) {
+    die("socket()");
+  }
+  int val = 1;
+  setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
+  
+  // bind
+  struct sockaddr_in addr = {};
+  addr.sin_family = AF_INET;
+  addr.sin_port = ntohs(1234);
+  addr.sin_addr.s_addr = ntohl(0);  // wildcard address 0.0.0.0
+  int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+  if (rv) {
+    die("bind()");
+  }
+  
+  // set the listen fd to nonblocking mode
+  fd_set_nb(fd);
+  
+  // listen
+  rv = listen(fd, SOMAXCONN);
+  if (rv) {
+    die("listen()");
+  }
+  
+  // a map of all client connections, keyed by fd
+  std::vector<Conn *> fd2conn;
+  // the event loop
+  std::vector<struct pollfd> poll_args;
+  while (true) {
+    // prepare the arguments of the poll()
+    poll_args.clear();
+    // put the listening sockets in the first position
+    struct pollfd pfd = {fd, POLLIN, 0};
+    poll_args.push_back(pfd);
+    // the rest are connection sockets
+    for (Conn *conn : fd2conn) {
+      if (!conn) {
+        continue;
+      }
+      // always poll() for error
+      struct pollfd pfd = {conn->fd, POLLERR, 0};
+      // poll() flags from the application's intent
+      if (conn->want_read) {
+        pfd.events |= POLLIN;
+      }
+      if (conn->want_write) {
+        pfd.events |= POLLOUT;
+      }
+      poll_args.push_back(pfd);
+    }
+  
+    // wait for readiness
+    int rv = poll(poll_args.data(), (nfds_t)poll_args.size(), -1);
+    if (rv < 0 && errno == EINTR) {
+      continue; // not an error
+    }
+    if (rv < 0) {
+      die("poll()");
+    }
+  
+    // handle the listening socket
+    if (poll_args[0].revents) {
+      if (Conn *conn = handle_accept(fd)) {
+        // put it into the map
+        if (fd2conn.size() <= (size_t)conn->fd) {
+          fd2conn.resize(conn->fd + 1);
+        }
+        assert(!fd2conn[conn->fd]);
+        fd2conn[conn->fd] = conn;
+      }
+    }
+  
+    // handle connection sockets
+    for (size_t i = 1; i < poll_args.size(); ++i) { // note: skip the 1st
+      uint32_t ready = poll_args[i].revents;
+      if (ready == 0} {
+        continue;
+      }
+      
+      Conn *conn = fd2conn[poll_args[i].fd];
+      if (ready & POLLIN) {
+        assert(conn->want_read);
+        handle_read(conn);  // application logic
+      }
+      if (ready & POLLOUT) {
+        assert(conn->want_write);
+        handle_write(conn); // application logic
+      }
+      
+      // close the socket from socket error or application logic
+      if ((ready & POLLERR) || conn->want_close) {
+        (void)close(conn->fd);
+        fd2conn[conn->fd] = NULL;
+        delete conn;
+      }
+    } // for each connection socket
+  } // the event loop
+  return 0;
+}
