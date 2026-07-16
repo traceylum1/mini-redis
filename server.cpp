c563f553574e86d26d1f855966d54721739e5bdd
Tracey Lum <traceylum@gmail.com>
2026-07-16 10:41:39 -0700

    implementing full read and write

--- server.cpp
+++ server.cpp
@@ -9,6 +9,7 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 
+#define MAX_MSG_LEN 4096
 
 static void msg(const char *msg) {
   fprintf(stderr, "%s\n", msg);
@@ -33,7 +34,55 @@ static void do_something(int connfd) {
   write(connfd, wbuf, strlen(wbuf));
 }
 
-void main() {
+static int32_t full_read(int connfd, void *rbuf, uint32_t n) {
+  // read expected number of bytes into rbuf
+  while (n > 0) {
+    ssize_t read_bytes = read(connfd, rbuf, n);
+    if (read_bytes <= 0) {
+      return -1; // Early EOF or error
+    }
+    // decrement bytes to read
+    n -= read_bytes;
+    if (n < 0) {
+      return -1; // Too many bytes read ?
+    }
+  }
+  // return success -- full msg read
+  return 0;
+}
+
+static int32_t full_write(int connf, void *rbuf) {
+  char wbuf[4 + MAX_MSG_LEN];
+}
+
+// handle_request will call full_read() twice -- once to get msg len, then to get msg body
+static int32_t handle_request(int connfd) {
+  char rbuf[4 + MAX_MSG_LEN];
+
+  // Read msg len to first 4 bytes of rbuf
+  int32_t err = full_read(connfd, rbuf, 4);
+  if (err) {
+    return -1;  // full_read error
+  }
+
+  // Get total msg len from first 4 bytes read
+  uint32_t msg_len = 0;
+  memcpy(&msg_len, rbuf, 4);
+
+  if (msg_len > MAX_MSG_LEN) {
+    msg("Message body too long");
+    return -1;
+  }
+
+  // Read msg body to rbuf
+  int32_t err = full_read(connfd, &rbuf[4], msg_len);
+  if (err) {
+    return -1;  // full_read error
+  }
+  
+}
+
+int main() {
   // Start listening socket
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
